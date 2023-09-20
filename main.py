dba3a766f2aa067e3fd2c15c38ddceb381038b1d
Lennard <lennardpolito@gmail.com>
2023-09-20 14:41:33 +0800

    Extract and Store Data in Text Files

--- main.py
+++ main.py
@@ -18,13 +18,28 @@ def extract(source):
     value = extractor.extract(source)["tours"]
     return value
 
+
 def send_email():
     print("Email was sent!")
+
+
+def store(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
+
+def read(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
 
 
 if __name__ == "__main__":
     scraped = scrape(URL)
     extracted = extract(scraped)
     print(extracted)
+
+    content = read(extracted)
     if extracted != "No upcoming tours":
-        send_email()
+        if extracted not in "data.txt":
+            store(extracted)
+            send_email()
