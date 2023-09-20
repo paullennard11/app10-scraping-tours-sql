71c926245747f67c598cfa4cdc7d2f28ba1d2982
Lennard <lennardpolito@gmail.com>
2023-09-20 14:54:33 +0800

    Send Email when New Event is Found on the Website and run the program non stop

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 import selectorlib
+import os
+import smtplib, ssl
+import time
 
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
@@ -19,7 +22,19 @@ def extract(source):
     return value
 
 
-def send_email():
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "lennadpolito@gmail.com"
+    password = os.getenv("PASSWORD")
+
+    receiver = "lennadpolito@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
     print("Email was sent!")
 
 
@@ -32,14 +47,15 @@ def read(extracted):
     with open("data.txt", "r") as file:
         return file.read()
 
-
 if __name__ == "__main__":
-    scraped = scrape(URL)
-    extracted = extract(scraped)
-    print(extracted)
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
 
-    content = read(extracted)
-    if extracted != "No upcoming tours":
-        if extracted not in "data.txt":
-            store(extracted)
-            send_email()
+        content = read(extracted)
+        if extracted != "No upcoming tours":
+            if extracted not in "data.txt":
+                store(extracted)
+                send_email(message="Hey, new event was found!")
+        time.sleep(2)
